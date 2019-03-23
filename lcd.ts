b1d5245a7be828211e98b747a346e53cc76ad485
JL <jan.lammertyn@gmail.com>
2019-03-23 17:08:44 +0100

    test fill value

--- lcd.ts
+++ lcd.ts
@@ -152,7 +152,7 @@ namespace makerbit {
     startPosition: number,
     endPosition: number
   ): void {
-    const whitespace = " ".charCodeAt(0);
+    const whitespace = "0".charCodeAt(0);
 
     for (
       let textPosition = 0;
